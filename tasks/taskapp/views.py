bb0cfcba00e4e7e04804209dbeffba52eaae51d8
Mohammed Ziya <mdziyacs44@gmail.com>
2023-01-24 21:45:17 +0530

    added

---
+++ tasks/taskapp/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from . models import Place,Team
+def demo(request):
+    obj=Place.objects.all()
+    obj1=Team.objects.all()
+    return render(request,"index.html",{'result':obj,'result1':obj1})
+
+
